55c2ad222b728775253459ec8f465631cd8c529a
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-12-10 17:19:12 +0530

    code

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,1 +1,11 @@
-console.log("ravi kumar");
+// console.log("ravi kumar");
+// var x=10;
+// console.log(x);
+// let y=12;
+// console.log(y);
+// const z=11;//const is always constant .
+// console.log(z);
+let a=5;
+let b="5";
+console.log(a===b);
+console.log(a==b);
