d6149bbb9c1934798b75788373b64f846409ddb9
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-12-18 13:40:23 +0530

    code

--- Javascript/first.js
+++ Javascript/first.js
@@ -10,7 +10,33 @@
 // console.log(a===b);// it compare value  and type both.
 // console.log(a==b);
 // console.log(typeof("ravi"));
-console.log(sum(2,3));
-function sum(x,y){
-    return x+y;
+// console.log(sum(2,3));
+// function sum(x,y){
+//     return x+y;
+// }
+
+// const arrowSum=(a,b)=>{
+//     console.log(a+b);
+// };
+// arrowSum(3,4);
+
+// const printHello=()=>{
+//     console.log("hello world");
+// };
+// printHello("my nae is ravi kumar");
+
+function count(str){
+    let count=0;
+    for(const char of str)
+        if(char==="a" || char==="e" || char==="i"){
+            count++;
+        }
+        console.log(count);
 }
+count("aeiouu");
+
+
+
+
+
+
