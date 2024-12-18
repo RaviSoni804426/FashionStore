2fb7a8ec4f934404bbed2b702842ea1ceef011a6
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2024-12-18 12:53:06 +0530

    code

--- Javascript/Youtube/script.js
+++ Javascript/Youtube/script.js
@@ -1,5 +1,14 @@
-function myfunction(msg){ //parameter
-    console.log(msg);
+// function myfunction(msg){ //parameter
+//     console.log(msg);
+// }
+// myfunction("my profile is the best "); //argument
+
+// function sum(a,b){
+//     console.log(a+b);
+// }
+// sum(2,3);
+// sum(4,2);
+function sum(a,b){
+    return a+b;
 }
-myfunction("my profile is the best "); //argument
 
