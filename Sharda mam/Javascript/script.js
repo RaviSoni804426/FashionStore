fba9702c9879bcc2777debd99c6d4e233296d934
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-04 19:48:18 +0530

    code

--- Sharda mam/Javascript/script.js
+++ Sharda mam/Javascript/script.js
@@ -1,4 +1,25 @@
-let div=document.querySelector("");
-console.log(param.setAttribute("class","new class"));
+// let div=document.querySelector("div");
+// div.style.color="pink";
+// div.style.backgroundColor="blue;"
+
+// let newBtn=document.createElement('button');
+// newBtn.textContent="click me!"
+// console.log(newBtn);
+
+// let div=document.querySelector("div");
+// div.prepend(newBtn);
 
 
+// let newHeading=document.createElement("h1");
+// newHeading.innerText="hardwork is the path of get success!";
+// document.querySelector("body").prepend(newHeading);
+
+
+// Project1
+// let newBtn=document.createElement("button");
+// newBtn.innerText="Click me";
+//  newBtn.style.color="white";
+//  newBtn.style.backgroundColor="red";
+
+//  document.querySelector("body").prepend(newBtn)
+ 
