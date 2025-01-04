88d28606e9e2394f8e41671de6f43be25fc2b8bf
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-04 19:16:50 +0530

    code

--- Sharda mam/Javascript/script.js
+++ Sharda mam/Javascript/script.js
@@ -1,6 +1,4 @@
-let div=document.querySelector("div");
-console.log(div);
+let div=document.querySelector("");
+console.log(param.setAttribute("class","new class"));
 
-let id=div.getAttribute("id");
-console.log(id);
 
