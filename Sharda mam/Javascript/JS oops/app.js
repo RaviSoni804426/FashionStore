ea1378fd1eeadb14f2effa4eca0d30bd5f376481
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-06 20:02:08 +0530

    refactor: update ToyotaCar class and adjust stylesheet link in index.html

--- Sharda mam/Javascript/JS oops/app.js
+++ Sharda mam/Javascript/JS oops/app.js
@@ -1,3 +1,21 @@
-const student={
-    
+// const student={
+//     fullName:"ravi kuamar",
+//     marks:92.2,
+//     printMarks: function () {
+//         console.log("marks= ",this.marks)
+//     },
+// };
+
+class ToyotaCar{
+    constructor(){
+        console.log("creating new object");
+    }
+
+    start(){
+        console.log("start");
+    }
+    stop(){
+       console.log("stop");
+    }
 }
+let fortuner=new ToyotaCar();
