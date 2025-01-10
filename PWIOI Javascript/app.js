71428b63780f8983524537dbb823946fdbf17992
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-10 13:39:49 +0530

    code

---
+++ PWIOI Javascript/app.js
@@ -0,0 +1,6 @@
+const anotherdiv=document.querySelector('#anotherdiv');
+anotherdiv.addEventListener('click',()=>anotherdiv.style.color="pink");
+
+function changeColor(){
+    document.getElementById("mydiv").style.color="red";
+}
