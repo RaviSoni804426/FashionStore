2a938c16200551f4485f21f926245208f593317d
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-10 13:09:05 +0530

    code

--- Sharda mam/Javascript/API/app.js
+++ Sharda mam/Javascript/API/app.js
@@ -1,5 +1,13 @@
-const URL="https://cat-fact.herokuapp.com/facts";
+const URL='https://cat-fact.herokuapp.com/facts';
 
 
 let promise=fetch(URL);
 console.log(promise);
+
+const getFacts=async()=>{
+    console.log("getting data..");
+    let response=await fetch(URL);
+    console.log(response);
+    let data= await response.json;
+    console.log(data);
+};
