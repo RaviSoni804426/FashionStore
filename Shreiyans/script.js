08ef2027f442d59851d205895f3209cce16dc6ee
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-12 22:09:16 +0530

    js code

--- Shreiyans/script.js
+++ Shreiyans/script.js
@@ -1,4 +1,20 @@
 
 //hoisting-variable and function are hoisted which means their decalration is moved on the top of code.
 
-var a=5;
+// var a;
+// console.log(a);
+
+// var a=[12,13];
+// var b=a;
+// b.pop();
+
+// for(var i=0;i<8;i++){
+//     console.log(i);
+// }
+
+// function hellobolo(){
+//     console.log("hello bolo");
+// }
+// hellobolo();
+
+
