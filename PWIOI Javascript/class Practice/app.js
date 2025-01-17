812268ae27efbe8fa46e8c7a261ce6879ef82e33
Ravi Kumar <148850477+RaviSoni804426@users.noreply.github.com>
2025-01-17 13:24:16 +0530

    js code

--- PWIOI Javascript/class Practice/app.js
+++ PWIOI Javascript/class Practice/app.js
@@ -1,6 +1,13 @@
-const anotherdiv=document.querySelector('#anotherdiv');
-anotherdiv.addEventListener('click',()=>anotherdiv.style.color="pink");
+// const anotherdiv=document.querySelector('#anotherdiv');
+// anotherdiv.addEventListener('click',()=>anotherdiv.style.color="pink");
 
-function changeColor(){
-    document.getElementById("mydiv").style.color="red";
+// function changeColor(){
+//     document.getElementById("mydiv").style.color="red";
+// }
+
+function(data,value){
+    this.data=a;
+    this.value=b;
+console.log(a,b)
 }
+function(2,3);
